d672847ef7bb0c8c52afdab0849a868905bd9602
qcloud <ubuntu@localhost.localdomain>
2023-02-13 23:26:21 +0800

    默认不编译QT,兼容VS2010问题

--- src/public/extra_data.hpp
+++ src/public/extra_data.hpp
@@ -4,6 +4,11 @@
 #include "public/define.h"
 #include "public/string.h"
 #include "util/map.hpp"
+#if defined(_WIN32) && defined(MSVC_2010)
+#define EXTRA_DATA_VALUE nstring
+#else
+#define EXTRA_DATA_VALUE std::any
+#endif
 #include <any>
 namespace newobj
 {
@@ -21,14 +26,21 @@ namespace newobj
         {
 
         }
-        void extra(const nstring& name, const std::any& data){
+        void extra(const nstring& name, const EXTRA_DATA_VALUE& data){
             m_map.set(name,data,true);
         }
+#ifndef MSVC_2010
         template<typename T>
-        T extra(const nstring& name){
-            std::any data;
+        T 
+#endif
+        extra(const nstring& name){
+            EXTRA_DATA_VALUE data;
             m_map.get(name, data);
+#ifdef MSVC_2010
+            return data;
+#else
             return std::any_cast<T>(data);
+#endif
         }
 	private:
         newobj::map<nstring,std::any> m_map;
