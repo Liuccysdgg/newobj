e028c70c3e460f48643199c0fa66f8dc8434f580
qcloud <ubuntu@localhost.localdomain>
2023-02-14 01:23:23 +0800

    整理HPSocket相关

--- include/hpsocket/hpsocket.h
+++ include/hpsocket/hpsocket.h
@@ -2,10 +2,7 @@
 #include "public/define.h"
 
 #ifdef _WIN32
-#include "hpsocket/windows/HPSocket.h"
-#include "hpsocket/windows/HPSocket-SSL.h"
-#else
-#include "hpsocket/linux/HPSocket.h"
-#include "hpsocket/linux/HPSocket-SSL.h"
+#include "hpsocket/HPSocket.h"
+#include "hpsocket/HPSocket-SSL.h"
 #endif
 
